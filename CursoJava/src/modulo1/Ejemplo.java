1729e7fba24e9945ba0b4efef1024be7b8e4e887
Jordi <jordi.guirao@allianz.es>
2022-05-25 14:12:35 +0200

    Primera practica Java v2

--- CursoJava/src/modulo1/Ejemplo.java
+++ CursoJava/src/modulo1/Ejemplo.java
@@ -6,6 +6,13 @@ public class Ejemplo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
 		System.out.println("\nhola\nadios");
+		System.out.println("\n\n\nnombre\t\tapellido\tlink git");
+		System.out.println("\nJordi\t\tGuirao\t\thttps://github.com/jordi-guirao/CursoJava2022");
+		System.out.println("\nXavi\t\tDomingo\t\thttps://github.com/XaviD2022/CursoJava2022");
+		System.out.println("\nJordi\t\tDonada\t\thttps://github.com/jdonada/fromcoboltojava");
+		System.out.println("para las comillas \" ");
+		System.out.println("para las barras \\ ");
+
 	}
 
 }
