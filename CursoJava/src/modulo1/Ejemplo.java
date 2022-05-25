b95e36d0a6d558e334a22de0db9a94dd87db66ad
Jordi <jordi.guirao@allianz.es>
2022-05-25 13:53:39 +0200

    Primera practica Java

---
+++ CursoJava/src/modulo1/Ejemplo.java
@@ -0,0 +1,11 @@
+package modulo1;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		System.out.println("\nhola\nadios");
+	}
+
+}
