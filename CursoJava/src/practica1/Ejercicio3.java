318e0ad6a611e0f6e9f3c4734a0a4884be94fea9
Jordi <jordi.guirao@allianz.es>
2022-05-26 13:47:52 +0200

    Correccion Ejercicio3 tabulaciones

--- CursoJava/src/practica1/Ejercicio3.java
+++ CursoJava/src/practica1/Ejercicio3.java
@@ -7,10 +7,10 @@ public class Ejercicio3 {
 		System.out.println("\nTecla de escape\t\tsignificado");
 		System.out.println("\n\\n \t\t\tsignifica nueva línia");
 		System.out.println("\\t \t\t\tsignifica un tab de espacio");	    
-        System.out.println("\\\" \t\t\tes para poner \" (comillas dobles) dentro del texto por ejemplo");
-        System.out.println("\t\t\t\"Belencita\"");
-        System.out.println("\\\\ \t\t\tse utiliza para escibir la \\ dentro del texto, por ejemplo \\algo\\");
-        System.out.println("\' \t\t\tse utiliza para las \'(comilla simple) para  escibir por ejemplo \'Princesita\'");
+		System.out.println("\\\" \t\t\tes para poner \" (comillas dobles) dentro del texto por ejemplo");
+		System.out.println("\t\t\t\"Belencita\"");
+		System.out.println("\\\\ \t\t\tse utiliza para escibir la \\ dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\' \t\t\tse utiliza para las \'(comilla simple) para  escibir por ejemplo \'Princesita\'");
 	
 
 	}
