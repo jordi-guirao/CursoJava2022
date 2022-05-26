efaff88d0122729338ea34007f50a9c9da23b2e2
Jordi <jordi.guirao@allianz.es>
2022-05-26 13:44:05 +0200

    Ejercicio 2 y 3 de la Praciva 1

---
+++ CursoJava/src/practica1/Ejercicio2.java
@@ -0,0 +1,19 @@
+package practica1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\nNombre\t\tapellido\temail \t\t\t\tteléfono");
+		System.out.println("\n------\t\t--------\t----- \t\t\t\t--------");
+		System.out.println("\n1 Gerard\tRodellas\tgerard.rodellas@allianz.es\t682705102");
+		System.out.println("\n2 Xavi\t\tDomingo\t\txavier.domingo@allianz.es\t638930005");
+		System.out.println("\n3 Oscar\t\tGou\t\toscar.gou@allianz.es\t\t628112609");
+		
+		
+
+	
+	}
+
+}
